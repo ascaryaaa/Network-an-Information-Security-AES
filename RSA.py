20e6650ebdd9df42e0f5331ad3da36712faa944f
hillmyhanif <73651220+hillmyhanif@users.noreply.github.com>
2022-04-05 10:38:15 +0700

    added timer

--- RSA.py
+++ RSA.py
@@ -4,6 +4,7 @@ from Cryptodome import Random
 from Cryptodome.Cipher import PKCS1_OAEP
 import binascii
 import ast
+import time
 
 random_generator = Random.new().read
 #generate pub and priv key
@@ -12,6 +13,8 @@ key = RSA.generate(1024, random_generator)
 #pub key export for exchange
 publickey = key.publickey() 
 encryptor = PKCS1_OAEP.new(publickey)
+
+start = time.time()
 
 #message to encrypt
 encrypted = encryptor.encrypt(b'this is a secret message that needs to be encrypted')
@@ -24,7 +27,11 @@ f = open ('RSA encrypted.txt', 'w')
 f.write(str(encrypted)) 
 f.close()
 
+end = time.time()
+print('\nEncryption Execution Time: ', end-start)
+
 #decrypted code below
+start2 = time.time()
 
 f = open('RSA encrypted.txt', 'r')
 message = f.read()
@@ -37,3 +44,6 @@ print('\n decrypted :', decrypted.decode('ascii'))
 f = open ('RSA decrypted.txt', 'w')
 f.write(str(decrypted.decode('ascii')))
 f.close()
+
+end2 = time.time()                                                                       
+print('\nDecryption Execution Time: ', end2-start2, '\n')
